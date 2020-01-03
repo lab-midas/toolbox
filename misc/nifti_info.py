46ba71af62aa820e16af581df3d8b9a1b5bcc0c7
tobias <tobias.hepp@med.uni-tuebingen.de>
2020-01-04 00:05:15 +0100

    Minor changes to nifti_info.py.

--- misc/nifti_info.py
+++ misc/nifti_info.py
@@ -3,14 +3,17 @@ import argparse
 from pathlib import Path
 
 def main():
-    print("NIFI-image information")
+    """ Shows basic information for nifti files.
+    (Size, Spacing, Origin, Direction)
 
+    Returns:
+
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--nii', help='image .nii file')
+    parser.add_argument('nii', help='image .nii file', required=True)
     args = parser.parse_args()
 
-    nii_file = vars(args)['nii']
-    print(nii_file)
+    nii_file = args.nii
     if not nii_file:
         print('please specifiy .nii file')
         return 1
@@ -27,6 +30,14 @@ def main():
     print('Spacing:   ', img.GetSpacing())
     print('Origin:    ', img.GetOrigin())
     print('Direction: ', img.GetDirection())
+
+    # todo switch one line / multi line output
+    print('Size {} Spacing {} Origin {} Direction {}'.format(img.GetSize(),
+                                                             img.GetSpacing(),
+                                                             img.GetOrigin(),
+                                                             img.GetDirection()))
+
+    # todo show metadata
     #print('MetaData:  ')
     #for key in img.GetMetaDataKeys():
     #    print(key, ': ', img.GetMetaData(key))
