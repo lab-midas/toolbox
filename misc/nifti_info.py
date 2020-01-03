a60481fd002c35149eedfd4bb7905463a69d0f32
tobias <tobias.hepp@med.uni-tuebingen.de>
2020-01-04 00:27:39 +0100

    nifti_info.py: Argument parsing/multiline output.

--- misc/nifti_info.py
+++ misc/nifti_info.py
@@ -9,39 +9,34 @@ def main():
     Returns:
 
     """
+
+    # Parse arguments.
     parser = argparse.ArgumentParser()
-    parser.add_argument('nii', help='image .nii file', required=True)
+    parser.add_argument('nii', help='.nii file')
+    parser.add_argument("-m", action="store_true", help="multi line output")
     args = parser.parse_args()
-
     nii_file = args.nii
-    if not nii_file:
-        print('please specifiy .nii file')
-        return 1
-    nii_file = Path(nii_file)
-    if not nii_file.exists():
-        print(str(nii_file), ' does not exist.')
 
+    # Load nii file.
     reader = sitk.ImageFileReader()
     reader.SetImageIO('NiftiImageIO')
     reader.SetFileName(str(nii_file))
     img = reader.Execute()
 
-    print('Size:      ', img.GetSize())
-    print('Spacing:   ', img.GetSpacing())
-    print('Origin:    ', img.GetOrigin())
-    print('Direction: ', img.GetDirection())
-
     # todo switch one line / multi line output
-    print('Size {} Spacing {} Origin {} Direction {}'.format(img.GetSize(),
-                                                             img.GetSpacing(),
-                                                             img.GetOrigin(),
-                                                             img.GetDirection()))
+    template = ' Size: {}{} Spacing: {}{} Origin: {}{} Direction: {}'
+    sep = ''
+    if args.m:
+        sep = '\n'
+    print(template.format(img.GetSize(), sep,
+                          img.GetSpacing(), sep,
+                          img.GetOrigin(), sep,
+                          img.GetDirection()))
 
     # todo show metadata
-    #print('MetaData:  ')
+    #print('MetaData:  '
     #for key in img.GetMetaDataKeys():
     #    print(key, ': ', img.GetMetaData(key))
-
 
 if __name__ == '__main__':
     main()
