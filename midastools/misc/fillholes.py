edfdb096d1b4416eccd76f3edb30e220589ef878
Tobias Hepp <tobias.hepp@med.uni-tuebingen.de>
2020-03-29 11:06:34 +0000

    fillholes/smoothin revised

--- midastools/misc/fillholes.py
+++ midastools/misc/fillholes.py
@@ -5,29 +5,16 @@ import argparse
 import numpy as np
 
 
-def main():
-    # Commandline argument parsing.
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('nii_input', help='Input .nii file')
-    parser.add_argument('-p', '--Praefix', help='Praefix for output filename')
-    parser.add_argument('-o', '--Output', help='Output .nii file')
-    args = parser.parse_args()
+def fillholes(nii_file,
+              outpath):
+    """Binary fill holes using scipy.ndimage
 
-    nii_file = Path(args.nii_input)
+    Args:
+        nii_file (str/Path): input nii mask file
+        outpath (str/Path): output path to save modified file
+    """
 
-    print('filling mask holes, nii image : ', str(nii_file))
-
-    # todo test if nii file exists
-
-    # If outpath is not set, use praefix and input filepath.
-    outpath = args.Output
-    if not outpath:
-        praefix = 'filled_'
-        if args.Praefix:
-            praefix = args.Praefix
-        outpath = nii_file.parent.joinpath(praefix + nii_file.name)
-
-    # Read nii image.
+     # Read nii image.
     label = sitk.ReadImage(str(nii_file))
     print(label.GetSize())
 
@@ -48,6 +35,29 @@ def main():
     print("Writing output to :", outpath)
     writer.SetFileName(str(outpath))
     writer.Execute(filled_label)
+
+
+def main():
+    # Commandline argument parsing.
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('nii_input', help='Input .nii file')
+    parser.add_argument('-p', '--Praefix', help='Praefix for output filename')
+    parser.add_argument('-o', '--Output', help='Output .nii file')
+    args = parser.parse_args()
+
+    nii_file = Path(args.nii_input)
+
+    print('filling mask holes, nii image : ', str(nii_file))
+
+    # If outpath is not set, use praefix and input filepath.
+    outpath = args.Output
+    if not outpath:
+        praefix = 'filled_'
+        if args.Praefix:
+            praefix = args.Praefix
+        outpath = nii_file.parent.joinpath(praefix + nii_file.name)
+
+    fillholes(nii_file, outpath):
 
 if __name__ == '__main__':
     main()
--- midastools/misc/smoothing.py
+++ midastools/misc/smoothing.py
@@ -1,25 +1,26 @@
-from med_shape.utils_vtk import vtk_conversion, vtk_mesh
+from midastools.vtk import vtk_conversion, vtk_mesh
 import SimpleITK as sitk
 import numpy as np
 import argparse
 from pathlib import Path
 
 
-def smooth_img(img,
+def smooth_img(nii_file,
+               outpath,
                smooth_filter='sinc',
                smooth_iter=40,
                relaxation=0.2):
-    """
-    Smooths input sitk image and return smoothed volume as sitk image.
+    """Smooths input nii file and saves the smoothed volume.
+    
     Args:
-        img: sitk image
+        nii_file (str/Path]): input nii mask file
+        outpath (str/Path): output path to save modified file
         smooth_filter: 'sinc' oder 'laplacian'
         smooth_iter: laplacian smoothing parameters
         relaxation: laplacian smoothing parameters
 
-    Returns: sitk image
-
     """
+    img = sitk.ReadImage(str(nii_file))
     volume = sitk.GetArrayFromImage(img)
     volume = volume.transpose([2, 1, 0])
     vtk_data = vtk_conversion.np_to_vtk_data(volume)
@@ -42,7 +43,7 @@ def smooth_img(img,
     img_result.SetOrigin(img.GetOrigin())
     img_result.SetSpacing(img.GetSpacing())
 
-    return img_result
+    sitk.WriteImage(img_result, str(outpath))
 
 
 def main():
@@ -65,7 +66,7 @@ def main():
     nii_file = Path(args.nii_input)
 
     print(f'Smoothing nii image : {str(nii_file)} ...')
-    # todo test if nii file exists
+
     out_file = args.Output
     if not out_file:
         praefix = 'smooth_'
@@ -85,10 +86,8 @@ def main():
     if args.relaxation:
         relaxation = args.relaxation
 
-    img = sitk.ReadImage(str(nii_file))
-    img_result = smooth_img(img,
-               smooth_filter, smooth_iter, relaxation)
-    sitk.WriteImage(img_result, str(out_file))
+    smooth_img(img, smooth_filter, smooth_iter, relaxation)
+
 
 if __name__ == '__main__':
     main()
